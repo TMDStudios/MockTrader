340d23c6a2a22228da08bc891cf203e3a1d0e0aa
TMDStudios <tmdstudioscontact@gmail.com>
2022-06-13 08:40:02 -0400

    Initial Commit

---
+++ app/src/main/java/com/tmdstudios/mocktrader/FragmentHome.kt
@@ -0,0 +1,31 @@
+package com.tmdstudios.mocktrader
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class FragmentHome : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.findViewById<Button>(R.id.btStart).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragmentHome_to_fragmentTrader)
+        }
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/tmdstudios/mocktrader/FragmentTrader.kt
@@ -0,0 +1,32 @@
+package com.tmdstudios.mocktrader
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class FragmentTrader : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_trader, container, false)
+
+        view.findViewById<Button>(R.id.btHome).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragmentTrader_to_fragmentHome)
+        }
+
+        return view
+    }
+
+}
