c238925508ed513a297942be2e4f552965b26670
TMDStudios <tmdstudioscontact@gmail.com>
2022-06-14 00:11:16 -0400

    Added GitHub link to project

--- app/src/main/java/com/tmdstudios/mocktrader/FragmentHome.kt
+++ app/src/main/java/com/tmdstudios/mocktrader/FragmentHome.kt
@@ -1,11 +1,14 @@
 package com.tmdstudios.mocktrader
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.cardview.widget.CardView
 import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,6 +17,7 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class FragmentHome : Fragment() {
+    private lateinit var cvGit: CardView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +28,13 @@ class FragmentHome : Fragment() {
 
         view.findViewById<Button>(R.id.btStart).setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_fragmentHome_to_fragmentTrader)
+        }
+
+        cvGit = view.findViewById(R.id.cvGit)
+        cvGit.setOnClickListener {
+            val uri = Uri.parse("https://github.com/TMDStudios/MockTrader")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
         }
 
         return view
